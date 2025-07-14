4b372e60c8140f2935bb35ea177df6c60ec4b422
peds <hugh1127@gmail.com>
2025-07-14 15:33:46 +0900

    feat: introduce turborepo

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -2,11 +2,13 @@ import js from "@eslint/js";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
 import unusedImports from "eslint-plugin-unused-imports";
 import tseslint from "typescript-eslint";
+import turbo from "eslint-plugin-turbo";
 
 /** @type {import("eslint").Linter.Config[]} */
 export default [
   js.configs.recommended,
   ...tseslint.configs.recommended,
+  turbo.configs["flat/recommended"],
   {
     plugins: {
       "simple-import-sort": simpleImportSort,
@@ -24,6 +26,7 @@ export default [
       ],
       "simple-import-sort/imports": "error",
       "simple-import-sort/exports": "error",
+      "turbo/no-undeclared-env-vars": "off",
     },
   },
 ];
