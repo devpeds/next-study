0a134425ffc99ee00f56dcadf41c10fd7b0a74d2
peds <hugh1127@gmail.com>
2025-07-16 20:21:13 +0900

    fix(ui): prevent overflow for mobile screen width
    - Fix misconfiguration for @shared/ui#build

--- packages/ui/lib/next/components/layout.tsx
+++ packages/ui/lib/next/components/layout.tsx
@@ -19,7 +19,9 @@ export function Layout({
   ...navBarProps
 }: LayoutProps) {
   return (
-    <body className={`${sansVariable} ${monoVariable} antialiased`}>
+    <body
+      className={`${sansVariable} ${monoVariable} antialiased overflow-x-hidden`}
+    >
       <div className="relative flex flex-col min-h-screen font-sans">
         <NavBar {...navBarProps} LinkComponent={Link} />
         <div className="flex-1 flex flex-col p-8">{children}</div>
